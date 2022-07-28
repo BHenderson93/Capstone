50b12d53686a70eaae1783b158c47708969984fd
bhenderson93 <brycehenderson1993@gmail.com>
2022-07-28 10:11:40 -0700

    Added header CSS

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,7 +2,7 @@ import { setUncaughtExceptionCaptureCallback } from "process";
 import * as React from "react";
 import { Link } from 'react-router-dom';
 import { AppState } from '../../../pages/App/App'
-
+import './Header.css'
 interface HeaderInt {
   app: AppState
   setApp:React.Dispatch<React.SetStateAction<any>>
@@ -10,32 +10,32 @@ interface HeaderInt {
 
 export default function Header({ app , setApp }: HeaderInt) {
   const [state , setState ] = React.useState(false)
+
   function logout(){
     localStorage.removeItem('token')
     setApp({...app , user:''})
   }
-  return (
-
-    <header className="bg-sky-600">
-      <nav className="flex flex-row justify-around">
+  return (<>
         {app.user ? (
-          <ul>
-            <h1>{app.user}</h1>
-            <Link to="/" >Home</Link>
-            <Link to="/profile" >Profile</Link>
-            <Link to="/moods" >Food Moods</Link>
-            <button onClick={logout}>Log Out</button>
-          </ul>
+        <header className="bg-sky-600">
+          <h1 className="">Hi, {app.user}!</h1>
+          <nav className="flex flex-row justify-around">
+              <Link to="/" >Home</Link>
+              <Link to="/profile" >Profile</Link>
+              <Link to="/moods" >Food Moods</Link>
+          </nav>
+          <button onClick={logout}>Log Out</button>
+        </header>
         ) : (
-          <ul>
-            <Link to="/" >Home</Link>
-            <Link to="/login" >Login</Link>
-            <Link to="/signup" >Signup</Link>
-          </ul>
+          <header className="bg-sky-600">
+            <nav className="flex flex-row justify-around">
+                <Link to="/" >Home</Link>
+                <Link to="/login" >Login</Link>
+                <Link to="/signup" >Signup</Link>
+            </nav>
+          </header>
         )
-        }
-      </nav>
-
-    </header>
+        }  
+    </>
   )
 }
