d9031d7405cfc87d377a96559ddf4a997bc3f508
bhenderson93 <brycehenderson1993@gmail.com>
2022-08-01 09:54:05 -0700

    Pre heroku test deployment commit

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -22,7 +22,7 @@ export default function Header({ app , setApp }: HeaderProps) {
           <h1 className="w-2/12">Hi, {app.user}!</h1>
           <nav className="flex flex-row justify-around">
               {app.moods.length === 0 ? null : <Link to="/" className="w-3/12 flex items-center justify-center py-5 bg-blue-400 text-white font-medium uppercase rounded hover:bg-slate-600  transition duration-150 ease-in-out">Find Food!</Link>}
-              <Link className="w-3/12 flex items-center justify-center py-5 bg-blue-400 text-white font-medium uppercase rounded hover:bg-slate-600  transition duration-150 ease-in-out" to="/favorites"  >Favorites</Link>
+{/*               <Link className="w-3/12 flex items-center justify-center py-5 bg-blue-400 text-white font-medium uppercase rounded hover:bg-slate-600  transition duration-150 ease-in-out" to="/favorites"  >Favorites</Link> */}
               <Link className="w-3/12 flex items-center justify-center py-5 bg-blue-400 text-white font-medium uppercase rounded hover:bg-slate-600  transition duration-150 ease-in-out" to="/moods" >Edit Moods</Link>
           </nav>
           <button className="flex items-center justify-center py-5 w-2/12 bg-red-600 text-white font-medium uppercase rounded hover:bg-red-900 transition duration-150 ease-in-out" onClick={logout}>Log Out</button>
